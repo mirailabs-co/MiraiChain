60a0a01194e019a53d8f03f87b415fac761d68fc
tonyfandev <96903283+tonyfandev@users.noreply.github.com>
2023-12-26 15:00:16 +0700

    Add logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import HomepageFeatures from "@site/src/components/HomepageFeatures";
 export default function Home(): JSX.Element {
 	const { siteConfig } = useDocusaurusContext();
 	return (
-		<Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
+		<Layout title={`Hello from ${siteConfig.title}`} description="Mirai Chain Document">
 			<main>
 				<HomepageFeatures />
 			</main>
